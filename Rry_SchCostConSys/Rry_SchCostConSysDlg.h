5d7cfd4c6a4efce94ec70bd56c8f38ad974d17fe
guohk <ghk1990@163.com>
2018-11-08 17:54:28 +0800

    新项目第一次提交

---
+++ Rry_SchCostConSys/Rry_SchCostConSysDlg.h
@@ -0,0 +1,32 @@
+
+// Rry_SchCostConSysDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CRry_SchCostConSysDlg 对话框
+class CRry_SchCostConSysDlg : public CDialogEx
+{
+// 构造
+public:
+	CRry_SchCostConSysDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_RRY_SCHCOSTCONSYS_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
